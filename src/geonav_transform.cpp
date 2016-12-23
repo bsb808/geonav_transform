0f1795bb7f9d1fb9a39516fac59cca26438b60bc
Bingham, Brian S <bsbingha@nps.edu>
2016-12-23 08:40:26 -0800

    first commit of source files - compiles, but blank

---
+++ src/geonav_transform.cpp
@@ -0,0 +1,9 @@
+//#include "geonav_transform/geonav_transform.h"
+#include "geonav_transform/navsat_conversions.h"
+
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+#include <XmlRpcException.h>
+
+#include <string>
+
---
+++ src/geonav_transform_node.cpp
@@ -0,0 +1,11 @@
+//#include "geonav_transform/geonav_transform.h"
+//#include <Eigen/Dense>
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "geonav_transform_node");
+
+  
+  return 0;
+}
